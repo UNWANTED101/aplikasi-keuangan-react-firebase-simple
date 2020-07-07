0d2603c3c48fd888f95e7d2cea12e477b71fbd35
UNWANTED101 <haritz942013@gmail.com>
2020-07-07 10:30:17 +0700

    firskakjfloiwajka  commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>ini H1</h1>
+      <h2>ini h2</h2>
+    </div>
+  );
+}
